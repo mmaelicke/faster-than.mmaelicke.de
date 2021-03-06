3432db0ff0afcc5b78c619dd7960671eaebbcd56
Mirko Mälicke <mirko@hydrocode.de>
2021-03-06 08:52:55 +0100

    rebuild input form

--- src/Page/Add-Form/Add-Run/Add-Run.tsx
+++ src/Page/Add-Form/Add-Run/Add-Run.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, FormControl, Snackbar, TextField } from '@material-ui/core';
+import { Button, createStyles, FormControl, Grid, makeStyles, Snackbar, TextField, Theme } from '@material-ui/core';
 import { Alert, Autocomplete } from '@material-ui/lab'
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -7,6 +7,7 @@ import 'firebase/firestore';
 import { Run } from '../../../models/run.model';
 import { connect } from 'react-redux';
 import { AppState } from '../../../models/app-state.model';
+import { DatePicker } from '@material-ui/pickers';
 
 interface AddRunProps {
     availableNames: string[];
@@ -16,6 +17,15 @@ const addDocument = (data: Run) => {
     return firebase.app().firestore().collection('runs').add(data)
 }
 
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        inputGrid: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        }
+    })
+);
 
 const AddRun: React.FC<AddRunProps> = ({availableNames}) => {
     // we need some state
@@ -43,37 +53,50 @@ const AddRun: React.FC<AddRunProps> = ({availableNames}) => {
         pace = '00:00';
     };
 
+    // get the classes
+    const classes = useStyles(); 
+
     return (
         <React.Fragment>
             <form noValidate>
-                <FormControl fullWidth>
-                    {/*<TextField label="Your Name"  error={name === ''} onChange={e => updateName(e.target.value)} value={name}/>*/}
-                    <Autocomplete freeSolo autoComplete autoHighlight 
-                        options={availableNames} 
-                        renderInput={(params) => <TextField {...params} label="Your Name" />}
-                        onChange={(_, value) => updateName('' + value as string)}
-                        value={name}
-                    />
-                </FormControl>
-                <FormControl>
-                    <TextField type="number" label="Distance [km]" error={distance === 0} onChange={e => updateDistance(Number(e.target.value))} value={distance}/>
-                </FormControl>
-                <FormControl>
-                    <TextField type="number" label="Time [min]" error={timeMin === 0} onChange={e => updateTime(Number(e.target.value))} value={timeMin}/>
-                </FormControl>
-                <FormControl fullWidth>
-                    <TextField 
-                        type="date"
-                        label="Date"
-                        value={date.toISOString().split('T')[0]}
-                        onChange={e => updateDate(new Date(e.target.value))}
-                    />    
-                </FormControl>
+                <Grid container spacing={3}>
+                    
+                    <Grid item xs={12} lg={4}>
+                        <DatePicker
+                            variant="static"
+                            openTo="date"
+                            views={['year', 'month', 'date']}
+                            value={date}
+                            onChange={d => updateDate(new Date(d as any))}
+                        />
+                    </Grid>
+                    
+                    <Grid item xs={12} lg={8} className={classes.inputGrid}>
+                        <FormControl fullWidth>
+                            {/*<TextField label="Your Name"  error={name === ''} onChange={e => updateName(e.target.value)} value={name}/>*/}
+                            <Autocomplete freeSolo autoComplete autoHighlight 
+                                options={availableNames} 
+                                renderInput={(params) => <TextField {...params} label="Your Name" />}
+                                onChange={(_, value) => updateName(value ? value : '')}
+                                onInputChange={(_, value) => updateName(value)}
+                                value={name}
+                            />
+                        </FormControl>
+                        <FormControl fullWidth>
+                            <TextField type="number" label="Distance [km]" error={distance === 0} onChange={e => updateDistance(Number(e.target.value))} value={distance}/>
+                        </FormControl>
+                        <FormControl fullWidth>
+                            <TextField type="number" label="Time [min]" error={timeMin === 0} onChange={e => updateTime(Number(e.target.value))} value={timeMin}/>
+                        </FormControl>
+
+                        <p>
+                            On {date.toLocaleDateString()} {name}  made {distance} km in {timeMin} min. 
+                            (Pace: {pace} [min/km])
+                        </p>
+                    </Grid>
+                
+                </Grid>
             </form>
-            <p>
-                On {date.toLocaleString()} {name}  made {distance} km in {timeMin} min. 
-                (Pace: {pace} [min/km])
-            </p>
 
             <Button color="primary" disabled={name === '' || distance === 0 || timeMin === 0}
                 onClick={() => addDocument({
